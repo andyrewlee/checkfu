eb9827bbe87780c41f8154a6e2cb319a2455f464
Andrew Lee <andyrewlee@gmail.com>
2025-09-07 21:44:20 -0700

    Add back previous ts and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,15 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-img-element": "off",
-    },
-  },
 ];
+
+export default eslintConfig;
